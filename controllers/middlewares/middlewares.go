8f45d75f1aecd58117ce1ef6c78a42f10ae6242e
flucas97 <feijolucas1997@gmail.com>
2020-08-06 00:40:17 -0300

    mix logic

--- controllers/middlewares/middlewares.go
+++ controllers/middlewares/middlewares.go
@@ -1,7 +1,7 @@
 package middlewares
 
 /*
-	// GET
+	// ROUTES
 
 	request has a jwt in a header or cookie?
 	- no -> redirect to /login
@@ -13,25 +13,17 @@ package middlewares
 			exists and if it is equal of what we have
 
 			- yes ->
-				route / login or /register?
+				is GET? AND	route / login or /register?
 					- yes -> redirect to / (already registered)
-			- no -> redirect to login and clear this token
-
-	// POST
-
-	request has a jwt in a header or cookie?
-	- no -> redirect to /login
-	- yes ->
-			auth := auth.New(name)
-			token := auth.GenerateToken()
-
-			check (GET) against Redis if KEY/VALUE (NAME/TOKEN)
-			exists and if it is equal of what we have
-
+			- no -> IS GET?
+					- yes -> redirect to login and clear this token
+					- no ->
 			- yes ->
 				route is / login or /register?
 					- yes -> redirect to / (already registered)
-					- no -> keep flow
+					- no -> keep flow if success
+						yes -> INSERT into Redis (NAME/TOKEN)
+						no -> failed register
 			- no -> redirect to login and clear this token
 
 */

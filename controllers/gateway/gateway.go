48636a32872ee24aa50b8def80f455694ec72dc3
flucas97 <feijolucas1997@gmail.com>
2020-08-06 22:40:38 -0300

    start gateway business logic

--- controllers/gateway/gateway.go
+++ controllers/gateway/gateway.go
@@ -1,11 +1,46 @@
 package gateway
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/flucas97/cng/cng-baguera-auth-api/utils/logger"
+	"github.com/gin-gonic/gin"
+)
 
 func Entry(c *gin.Context) {
-	/*
+	URI := c.Request.RequestURI
 
-		load all avaliable routes
+	switch c.Request.Method {
+	case http.MethodGet:
+		switch URI {
+		case "/login":
+			res, err := http.Post("/api/login", "Auth", c.Request.Body)
+			if err != nil {
 
-	*/
+			}
+			_ = res
+		case "/account-details":
+			// account-details
+		case "/cannabis":
+			// cannabis
+		}
+	case http.MethodPost:
+		switch URI {
+		case "/login":
+			// login
+		case "/account-details":
+			// account-details
+		case "/cannabis":
+			// cannabis
+		}
+	case http.MethodPatch:
+		switch URI {
+		case "/account":
+			// account-details
+		case "/account-details":
+			// cannabis
+		}
+	default:
+		logger.Info("no routes")
+	}
 }

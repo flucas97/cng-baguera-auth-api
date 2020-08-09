7a174ff9c04ab3a151baa2fb35569a56628dd1df
flucas97 <feijolucas1997@gmail.com>
2020-08-09 00:16:16 -0300

    remove service logic

--- controllers/middlewares/middlewares.go
+++ controllers/middlewares/middlewares.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/flucas97/cng/cng-baguera-auth-api/domain/auth"
+	"github.com/flucas97/cng/cng-baguera-auth-api/services/auth_service"
 	"github.com/flucas97/cng/cng-baguera-auth-api/utils/error_factory"
 	"github.com/flucas97/cng/cng-baguera-auth-api/utils/logger"
 	"github.com/gin-gonic/gin"
@@ -43,10 +44,15 @@ func Entry(c *gin.Context) {
 
 func allowedPath(reqToken []string, c *gin.Context) {
 	if len(reqToken) != 0 {
-
+		logger.Info(fmt.Sprint(reqToken[0]))
+		c.Abort()
+		return
 	} else {
 		switch c.Request.RequestURI {
 		case "/new-account":
+			var (
+				authService = auth_service.AuthService
+			)
 			switch c.Request.Method {
 			case http.MethodPost:
 				w, err := http.Post("http://localhost:8081/api/new-account", "Authorized", c.Request.Body)
@@ -61,21 +67,29 @@ func allowedPath(reqToken []string, c *gin.Context) {
 					return
 				}
 
-				auth := auth.New(nickName)
-
-				jwt, restErr := auth.GenerateJWT()
+				token := auth.New(nickName)
+				authService.AuthorizeAccount(w.Header.Get("nick_name"))
+				jwt, restErr := token.GenerateJWT()
 				if restErr != nil {
 					c.JSON(http.StatusInternalServerError, "error generating token, try again")
 					return
 				}
 
+				if err := auth_service.AuthService.CacheJWT(jwt); err != nil {
+
+				}
 				/*
 					save nickName(key) - jwt(value) into Redis
 
 				*/
-				c.Header("cng-auth", jwt)
+				c.Header("Authorization", jwt)
 				c.AbortWithStatusJSON(http.StatusCreated, "account successfully created")
 				return
+			}
+		case "/login":
+			switch c.Request.Method {
+			case http.MethodGet:
+
 			}
 		}
 	}
@@ -88,7 +102,7 @@ func ForbiddenPath(c *gin.Context) {
 }
 
 func getAccountName(w *http.Response) (string, bool) {
-	nickName := w.Header.Get("nick_name")
+	nickName := 
 	if nickName == "" {
 		return "", false
 	}

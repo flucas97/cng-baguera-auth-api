76189bed974779c3bbffacc674b9cd873ad8c1db
flucas97 <feijolucas1997@gmail.com>
2020-08-12 21:48:36 -0300

    implement set and get into Redis pool, checking if jwt passed as header is the same in database

--- domain/auth/auth_dao.go
+++ domain/auth/auth_dao.go
@@ -5,22 +5,28 @@ import (
 
 	"github.com/flucas97/cng/cng-baguera-auth-api/db/auth/redis_db"
 	"github.com/flucas97/cng/cng-baguera-auth-api/utils/error_factory"
+	"github.com/flucas97/cng/cng-baguera-auth-api/utils/logger"
 )
 
 func (token *Token) Authorize(ctx context.Context) *error_factory.RestErr {
-	err := redis_db.Client.Set(ctx, token.Name, token.Jwt, 0).Err()
+	_, err := redis_db.Client.Set(ctx, token.Name, token.Jwt, 0).Result()
 	if err != nil {
+		logger.Error("redis set error", err)
 		return error_factory.NewInternalServerError(err.Error())
 	}
 
 	return nil
 }
 
-func (token *Token) Validate(context.Context) (bool, *error_factory.RestErr) {
-	/*
-		val, err := redis_db.Client.Get(key).Result()
-		check if is any register of this account in Redis
-		then check if the jwt token is the same in Token.Jwt
-	*/
+func Validate(ctx context.Context, nickName string, jwtSent string) (bool, *error_factory.RestErr) {
+	validJWT, err := redis_db.Client.Get(ctx, nickName).Result()
+	if err != nil {
+		return false, error_factory.NewBadRequestError(err.Error())
+	}
+
+	if validJWT != jwtSent {
+		return false, error_factory.NewNotFoundError("jwt not valid")
+	}
+
 	return true, nil
 }
--- domain/auth/auth_dto.go
+++ domain/auth/auth_dto.go
@@ -1,10 +1,12 @@
 package auth
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/flucas97/cng/cng-baguera-auth-api/utils/error_factory"
+	"github.com/flucas97/cng/cng-baguera-auth-api/utils/logger"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -21,18 +23,37 @@ func New(name string) *Token {
 	return au
 }
 
-func (au *Token) GenerateJWT() *error_factory.RestErr {
+func (au *Token) GenerateJWT() (string, *error_factory.RestErr) {
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["nick_name"] = au.Name
 	claims["uuid"] = au.Uuid
 
 	jwtPure := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
 	jwt, err := jwtPure.SignedString([]byte(os.Getenv("API_SECRET")))
 	if err != nil {
-		return error_factory.NewInternalServerError("error generating token")
+		return "", error_factory.NewInternalServerError("error generating token")
+	}
+	au.Jwt = jwt
+
+	return jwt, nil
+}
+
+func GetNickNameFromJWT(tokens string) (string, *error_factory.RestErr) {
+	jwts, err := jwt.Parse(tokens, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("API_SECRET")), nil
+	})
+	if err != nil {
+		logger.Error("error parsing jwt", nil)
+		return "", error_factory.NewInternalServerError("error getting token informations, try again")
 	}
 
-	au.Jwt = jwt
-	return nil
+	claims := jwts.Claims.(jwt.MapClaims)
+	nickName := claims["nick_name"]
+
+	return (nickName).(string), nil
 }

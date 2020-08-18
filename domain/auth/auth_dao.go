f7f9677b3027348e9a20204bde4a16abe85de837
flucas97 <feijolucas1997@gmail.com>
2020-08-18 11:58:26 -0300

    configure query to save token in psql

--- domain/auth/auth_dao.go
+++ domain/auth/auth_dao.go
@@ -3,7 +3,6 @@ package auth
 import (
 	"context"
 	"strings"
-	"time"
 
 	"github.com/flucas97/cng/cng-baguera-auth-api/db/auth/psql_db"
 	"github.com/flucas97/cng/cng-baguera-auth-api/db/auth/redis_db"
@@ -12,18 +11,18 @@ import (
 )
 
 const (
-	queryNewCannabis = ("INSERT INTO auth (account_id, jwt, nickname) VALUES ($1, $2, $3);")
-	queryShowDetails = ("SELECT jwt FROM auth WHERE nickname=$1;")
+	queryNewAccount = ("INSERT INTO auth (account_id, jwt, nickname) VALUES ($1, $2, $3);")
+	querySearchJwt  = ("SELECT jwt FROM auth WHERE nickname=$1;")
 )
 
 func (token *Token) Authorize(ctx context.Context) *error_factory.RestErr {
-	_, err := redis_db.Client.Set(ctx, token.Name, token.Jwt, 600000*time.Second).Result()
+	_, err := redis_db.Client.Set(ctx, token.Name, token.Jwt, 0).Result()
 	if err != nil {
 		logger.Error("redis set error", err)
 		return error_factory.NewInternalServerError(err.Error())
 	}
 
-	_ = psql_db.Client.QueryRow(queryNewCannabis, token.AccountId, token.Jwt, token.Name)
+	_ = psql_db.Client.QueryRow(queryNewAccount, token.AccountId, token.Jwt, token.Name)
 
 	return nil
 }
@@ -31,7 +30,7 @@ func (token *Token) Authorize(ctx context.Context) *error_factory.RestErr {
 func Validate(ctx context.Context, nickName string, jwtSent string) (bool, *error_factory.RestErr) {
 	validJWT, err := redis_db.Client.Get(ctx, nickName).Result()
 	if err != nil {
-		err = psql_db.Client.QueryRow(queryShowDetails, nickName).Scan(&validJWT)
+		err = psql_db.Client.QueryRow(querySearchJwt, nickName).Scan(&validJWT)
 		if err != nil {
 			if strings.Contains(err.Error(), "no rows in result set") {
 				return false, error_factory.NewNotFoundError("account not found")
